4558cdb712559f36b7c4d93ce8cdba08ae7461f4
Brad Gibson <b2b@humanenginuity.com>
2018-03-16 11:44:22 -0700

    unified datatype; added atomic::relaxed and unsynchronized test cases for comparison

--- src/lock_bench.rs
+++ src/lock_bench.rs
@@ -2,9 +2,9 @@ use std::thread;
 use std::sync::{Arc, Mutex, RwLock};
 use std::sync::atomic::{Ordering, AtomicUsize};
 
-const N: i32 = 10000;
+const N: usize = 10000;
 
-pub fn with<T, G>(num_reader: i32, num_writer: i32, gen: G, write: fn(&Arc<T>), read: fn(&Arc<T>))
+pub fn with<T, G>(num_reader: usize, num_writer: usize, gen: G, write: fn(&Arc<T>), read: fn(&Arc<T>))
     where T: Sync + Send + 'static, G: Fn() -> T {
     let mut ths = Vec::new();
     let counter = Arc::new(gen());
@@ -17,7 +17,7 @@ pub fn with<T, G>(num_reader: i32, num_writer: i32, gen: G, write: fn(&Arc<T>), 
             }
         }));
     }
-    
+
     for _ in 0..num_reader {
         let counter = counter.clone();
         ths.push(thread::spawn(move || {
@@ -26,39 +26,39 @@ pub fn with<T, G>(num_reader: i32, num_writer: i32, gen: G, write: fn(&Arc<T>), 
             }
         }));
     }
-    
+
     for th in ths {
         th.join().unwrap();
     }
 }
 
-pub fn with_mutex(num_reader: i32, num_writer: i32) {
-    fn write(counter: &Arc<Mutex<i32>>) {
+pub fn with_mutex(num_reader: usize, num_writer: usize) {
+    fn write(counter: &Arc<Mutex<usize>>) {
          let mut counter_ref = counter.lock().unwrap();
          *counter_ref += 1;
     }
 
-    fn read(counter: &Arc<Mutex<i32>>) {
+    fn read(counter: &Arc<Mutex<usize>>) {
          let _ = counter.lock().unwrap();
     }
 
     with(num_reader, num_writer, || Mutex::new(0), write, read);
 }
 
-pub fn with_rwlock(num_reader: i32, num_writer: i32) {
-    fn write(counter: &Arc<RwLock<i32>>) {
+pub fn with_rwlock(num_reader: usize, num_writer: usize) {
+    fn write(counter: &Arc<RwLock<usize>>) {
         let mut counter_ref = counter.write().unwrap();
         *counter_ref += 1;
     }
 
-    fn read(counter: &Arc<RwLock<i32>>) {
+    fn read(counter: &Arc<RwLock<usize>>) {
         let _ = counter.read().unwrap();
     }
 
     with(num_reader, num_writer, || RwLock::new(0), write, read);
 }
 
-pub fn with_atomic(num_reader: i32, num_writer: i32) {
+pub fn with_atomic_seq_cst(num_reader: usize, num_writer: usize) {
     fn write(counter: &Arc<AtomicUsize>) {
         counter.fetch_add(1, Ordering::SeqCst);
     }
@@ -70,3 +70,30 @@ pub fn with_atomic(num_reader: i32, num_writer: i32) {
     with(num_reader, num_writer, || AtomicUsize::new(0), write, read);
 }
 
+pub fn with_atomic_relaxed(num_reader: usize, num_writer: usize) {
+    fn write(counter: &Arc<AtomicUsize>) {
+        counter.fetch_add(1, Ordering::Relaxed);
+    }
+
+    fn read(counter: &Arc<AtomicUsize>) {
+        let _ = counter.load(Ordering::Relaxed);
+    }
+
+    with(num_reader, num_writer, || AtomicUsize::new(0), write, read);
+}
+
+// For performance reference only -- expect garbage results due to sync violations
+pub fn with_unsynchronized(num_reader: usize, num_writer: usize) {
+    fn write(counter: &Arc<usize>) {
+        use std::borrow::Borrow;
+        unsafe {
+            *std::mem::transmute::<&usize, *mut usize>(Arc::borrow(counter)) += 1;
+        }
+    }
+
+    fn read(counter: &Arc<usize>) {
+        let _ = counter;
+    }
+
+    with(num_reader, num_writer, || usize::default(), write, read);
+}
